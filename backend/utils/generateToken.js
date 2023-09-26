f7b6b896d74bdf1933f1b70a13636073e30ff293
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-25 20:57:18 -0500

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 // generate token that expires in 12 hours
 const generateToken = (id) => {
-  return jwt.sign({ id }, 'MIIJKAIBAAKCAgEAlUAvcSUJqqMn7PmWFd/EB4ZLh5f1Yl3SaR', { expiresIn: '12h' })
+  return jwt.sign({ id },process.env.JWT_SECRET, { expiresIn: '12h' })
 }
 
 export default generateToken
