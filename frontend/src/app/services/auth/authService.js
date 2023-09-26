5bc6c90c3752eac828bb2f6f311f77f8d0aff740
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-25 22:30:23 -0500

    Add files via upload

---
+++ frontend/src/app/services/auth/authService.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const backendURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000/'
+    : import.meta.env.VITE_SERVER_URL
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: backendURL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.userToken
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  endpoints: (build) => ({
+    getUserProfile: build.query({
+      query: () => ({
+        url: 'api/user/profile',
+        method: 'GET',
+      }),
+    }),
+    deposit: build.mutation({
+      query: (amount) => ({
+        url: 'api/user/deposit',
+        method: 'POST',
+        body: { amount },
+      }),
+    }),
+    withdraw: build.mutation({
+      query: (amount) => ({
+        url: 'api/user/withdraw',
+        method: 'POST',
+        body: { amount },
+      }),
+    }),
+  }),
+})
+
+// export react hooks
+export const { useGetUserProfileQuery, useDepositMutation, useWithdrawMutation } = authApi
