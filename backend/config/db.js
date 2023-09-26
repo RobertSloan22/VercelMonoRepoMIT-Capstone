94348c8ff168fd62eaeef7f92b044483d3c91982
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-25 20:56:31 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import { mongoose } from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://rstechsolutionsmeco:mitcapstone2023@cluster0.5qgxggh.mongodb.net/?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     })
