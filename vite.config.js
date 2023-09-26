b1f3dbeb494e8582f069b84088ef1e2276d65997
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-26 11:20:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-export default defineConfig(() => {
-  return {
-    define: {
-      __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    },
-  };
-});
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  base: '/',  
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), svgr()],
