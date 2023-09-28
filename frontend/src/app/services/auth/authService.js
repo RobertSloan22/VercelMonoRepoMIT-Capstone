64ddbd23665a1253722464ceb7e1d3c82e33d527
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-27 20:15:12 -0500

    Update authService.js

--- frontend/src/app/services/auth/authService.js
+++ frontend/src/app/services/auth/authService.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const backendURL =
   process.env.NODE_ENV !== 'production'
-    ?'https://oceanconnect-a532f183b3af.herokuapp.com/''
+    ?'https://localhost:5000'
     : import.meta.env.VITE_SERVER_URL
 
 export const authApi = createApi({
